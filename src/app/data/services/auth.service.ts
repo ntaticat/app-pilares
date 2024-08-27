8c69800479aea9fb15bd2d04589313103af7f7d7
Rafael Estrada <ntaticat@gmail.com>
2024-08-27 15:50:33 -0600

    add environments

--- src/app/data/services/auth.service.ts
+++ src/app/data/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://octopus-app-o7oh5.ondigitalocean.app/api',
+};
